c6b69488b02514d8238fcfa76bc7f9c5c7cb5076
Ian <ianselbyuk@gmail.com>
2024-01-20 13:10:57 +0000

    Adds GuessInput functionality and Validation.

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { useState }  from 'react';
 
 function GuessInput() {
+
+
+  [guess, setGuess] = React.useState('')
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log({guess});
+    setGuess('');
+  }
+
   return (
-    <form class="guess-input-wrapper">
-      <label class="guess-input-wrapper label">Enter Guess:</label>
-      <input type="text" class="guess-input-wrapper input"></input>
+    <form 
+      className="guess-input-wrapper"
+      onSubmit={handleSubmit}
+    >
+      <label className="guess-input-wrapper label" htmlFor="guess-input">Enter Guess:</label>
+      <input 
+        type="text" 
+        id="guess-input" 
+        className="guess-input-wrapper input"
+        minLength="5"
+        maxLength="5"
+        value={guess}
+        onChange={(e) => setGuess(e.target.value.toUpperCase())}
+      ></input>
     </form>
   );
 }

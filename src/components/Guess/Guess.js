e1f3c494cfdb11d4fb6ce597cc6867ce2b0f99e9
Ian <ianselbyuk@gmail.com>
2024-01-25 22:40:00 +0000

    Adds guess check logic and styles.

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -5,12 +5,32 @@ import {checkGuess} from '../../game-helpers'
 
 function Guess( {value, answer} ) {
   console.log(value)
-  console.log(answer)
+
+  const checkResults = checkGuess(value, answer)
+
+  //Josh' solution creates a separate Cell component within this component which he says is ok as it is only used in this component
+
+  // function Cell( { letter, status} ) {
+  //   const className = status ? `cell ${statugirs}` : `cell`
+  //   return <span className={className}>{letter}</span>
+  // }
+
+  // return (
+  //   <p className="guess">
+  //     {range(5).map((num) => (
+  //       <Cell
+  //         key={num}
+  //         letter={value ? checkResults[num].letter : undefined}
+  //         status={value ? checkResults[num].status : undefined}
+  //       />
+  //     ))}
+  //   </p>
+  // )
 
   return (
     <p className="guess">
       {range(5).map(num => (
-        <span key={num} className="cell">{value ? value[num] : undefined}</span>
+        <span key={num} className={value ? `cell ${checkResults[num].status}` : `cell`}>{value ? value[num] : null}</span>
       ))}
     </p>
   );

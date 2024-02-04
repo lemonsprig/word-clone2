d550d59d039e629085b5ce889a1b20403e11615d
Ian <ianselbyuk@gmail.com>
2024-02-04 16:47:30 +0000

    Add gameState

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -6,6 +6,8 @@ import GuessInput from '../GuessInput/GuessInput';
 import GuessList from '../GuessList/GuessList';
 import Guess from '../Guess/Guess';
 
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants'
+
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -15,14 +17,28 @@ console.info({ answer });
 function Game() {
 
   const [guessList, setGuessList] = React.useState(["woman", "lover","cakes"])
+  // game states allowed playing, won, lost
+  const [gameState, setGameState] = React.useState('playing')
 
   function handleSubmitGuess(guess) {
+
+    const newGuessList = [...guessList, guess]
+    console.log(newGuessList)
+
+
+    if(guess === answer) {
+      setGameState('won')
+    } else if(newGuessList.length >= NUM_OF_GUESSES_ALLOWED) {
+      setGameState('lost')
+    }
+
     setGuessList([...guessList, guess])
     console.log("game: ",{guessList})
   }
 
 
   return <>
+    {gameState}
     <GuessList guesses={guessList} answer={answer}></GuessList>
     <GuessInput guessList={guessList} handleSubmitGuess={handleSubmitGuess}></GuessInput>
   </>;
